3f4ae1d1ea0c63bf078b17635181b839adb3c06e
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-07-31 09:52:48 -0600

    Agregué el Main y las funciones para Agregar Receta y Gestor de Recetas

--- Actividas 8/Program.cs
+++ Actividas 8/Program.cs
@@ -46,7 +46,7 @@ namespace GestorDeRecetas
                 foreach (var receta in recetas)
                 {
                     Console.WriteLine(receta);
-                    Console.WriteLine(); 
+                    Console.WriteLine();
                 }
             }
             else
@@ -54,6 +54,84 @@ namespace GestorDeRecetas
                 Console.WriteLine("No hay recetas registradas.");
             }
         }
-    }
+        class Programa
+        {
+            static void Main(string[] args)
+            {
+                GestorDeRecetas gestor = new GestorDeRecetas();
+                bool ejecutando = true;
 
+                while (ejecutando)
+                {
+                    Console.WriteLine("Gestor de Recetas");
+                    Console.WriteLine("1. Agregar Receta");
+                    Console.WriteLine("2. Buscar Receta por Nombre");
+                    Console.WriteLine("3. Listar Todas las Recetas");
+                    Console.WriteLine("4. Salir");
+                    Console.Write("Elija una opción: ");
+
+                    int opcion;
+                    if (int.TryParse(Console.ReadLine(), out opcion))
+                    {
+                        switch (opcion)
+                        {
+                            case 1:
+                                AgregarReceta(gestor);
+                                break;
+                            case 2:
+                                BuscarRecetaPorNombre(gestor);
+                                break;
+                            case 3:
+                                gestor.ListarTodasLasRecetas();
+                                break;
+                            case 4:
+                                ejecutando = false;
+                                Console.WriteLine("Saliendo...");
+                                break;
+                            default:
+                                Console.WriteLine("Opción no válida. Por favor, intente nuevamente.");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrada no válida. Por favor, ingrese un número.");
+                    }
+                }
+            }
+
+
+        }
+        static void AgregarReceta(GestorDeRecetas gestor)
+        {
+            Console.Write("Ingrese el nombre de la receta: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese los ingredientes: ");
+            string ingredientes = Console.ReadLine();
+
+            Console.Write("Ingrese las instrucciones: ");
+            string instrucciones = Console.ReadLine();
+
+            Receta receta = new Receta(nombre, ingredientes, instrucciones);
+            gestor.AgregarReceta(receta);
+        }
+        static void BuscarRecetaPorNombre(GestorDeRecetas gestor)
+        {
+            Console.Write("Ingrese el nombre de la receta para buscar: ");
+            string nombre = Console.ReadLine();
+
+            Receta receta = gestor.BuscarRecetaPorNombre(nombre);
+            if (receta != null)
+            {
+                Console.WriteLine("Receta encontrada:");
+                Console.WriteLine(receta);
+            }
+            else
+            {
+                Console.WriteLine("Receta no encontrada.");
+            }
+        }
+
+    }
 }
