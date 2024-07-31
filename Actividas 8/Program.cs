774f75ddccd46010dc0ad519a741c8446bea9eed
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-07-31 09:22:49 -0600

    Cree la clase Receta

--- Actividas 8/Program.cs
+++ Actividas 8/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GestorDeRecetas
+{
+    public class Receta
+    {
+        public string Nombre { get; set; }
+        public string Ingredientes { get; set; }
+        public string Instrucciones { get; set; }
+
+        public Receta(string nombre, string ingredientes, string instrucciones)
+        {
+            Nombre = nombre;
+            Ingredientes = ingredientes;
+            Instrucciones = instrucciones;
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}\nIngredientes: {Ingredientes}\nInstrucciones: {Instrucciones}";
+        }
+    }
+
+  
+}
